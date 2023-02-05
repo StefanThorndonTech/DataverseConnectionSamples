72062f815fcb9442728758cf8bec9c5e6478119e
Stefan William-Worrall <Stefan@thorndontech.onmicrosoft.com>
2023-02-06 09:42:53 +1300

    Initial Maui Blazor App Sample
    Initial Sample Check in/

---
+++ C#/MauiApp/MauiBlazorAppServerToServerSample/MauiBlazorAppServerToServerSample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiBlazorAppServerToServerSample;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ C#/MauiApp/MauiBlazorAppServerToServerSample/MauiBlazorAppServerToServerSample/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBlazorAppServerToServerSample;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
